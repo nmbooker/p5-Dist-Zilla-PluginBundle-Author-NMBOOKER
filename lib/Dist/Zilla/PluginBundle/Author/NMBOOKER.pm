394186ce499f3576fa698b155e8acbe0866a5425
Nick Booker <nmb@nickbooker.uk>
2015-11-21 16:58:53 +0000

    Create changes file and add license info

--- lib/Dist/Zilla/PluginBundle/Author/NMBOOKER.pm
+++ lib/Dist/Zilla/PluginBundle/Author/NMBOOKER.pm
@@ -126,14 +126,24 @@ It is roughly equivalent to:
   -type  = requires
   Test::More = 0.96
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-I've based this on a clone of L<Dist::Zilla::PluginBundle::Author::OpusVL>, except that I release to
-the real CPAN by default.
+I've based this on a clone of L<Dist::Zilla::PluginBundle::Author::OpusVL>,
+under the terms of the BSD license, so I hereby acknowledge their copyright.
 
-  Copyright (C) 2015 Nicholas Booker
-  Copyright (C) 2015 OpusVL
+Copyright (C) 2015, Nicholas Booker
+Copyright (C) 2015, OpusVL
 
-License is 3-clause BSD.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =cut
